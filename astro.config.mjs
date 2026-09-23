667a14482847ab70851af1f11e081dca59aa0001
Alessandro Marcolini <alessandromarcolini99@gmail.com>
2026-09-23 19:22:43 +0200

    Add wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,16 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 export default defineConfig({
   site: "https://marcolini.dev",
   integrations: [mdx(), sitemap()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   markdown: {
     shikiConfig: {
       themes: {
@@ -17,4 +21,6 @@ export default defineConfig({
       },
     },
   },
+
+  adapter: cloudflare(),
 });
